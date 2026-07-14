f6892681a731f455d247e8266e9031466fe02c9d
mumuniazeez <mumuniazeez99@gmail.com>
2026-07-14 02:30:15 +0100

    feat: update landing page layout and add Textarea component

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 export default function LandingPageLayout({
   children,
 }: {
@@ -5,9 +7,14 @@ export default function LandingPageLayout({
 }) {
   return (
     <>
-      <header className="flex items-center justify-between p-4 border-b-5">
-        <div>Logo</div>
-      </header>
+      {/* <header className="flex items-center justify-between p-4 border-b-5">
+        <div>
+          <h1 className="text-3xl">Logo</h1>
+        </div>
+        <div className="flex gap-x-4">
+          <Button>Signup</Button>
+        </div>
+      </header> */}
       <main>{children}</main>
     </>
   );
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,10 +1,22 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Sparkle } from "lucide-react";
 
 export default function Home() {
   return (
     <div>
-      <Button variant="neutral">Hello</Button>
+      <div className="flex my-50 flex-col items-center justify-center gap-4">
+        <h1 className="text-7xl max-w-4xl text-center font-bold mb-3">
+          Don&apos;t know what to build for your next YSWS?
+        </h1>
+        <p className="text-2xl">
+          Let&apos;s suggest what you should build for your next YSWS
+        </p>
+        <div className="flex gap-x-3 mt-3">
+          <Button variant="default" size={"lg"}>
+            <Sparkle /> Get Started
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
